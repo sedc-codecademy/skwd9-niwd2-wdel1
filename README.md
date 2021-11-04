dd3fd235beee94a76afb6951a630716d4985a5b9
unknown <filipbarak94@gmail.com>
2021-11-04 20:27:02 +0100

    adds cors package, + readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# TODO Server
+
+To run this server you will need:
+
+1. Mongodb on your machine
+2. Node.js runtime
+
+To run, use commands:
+
+1. `npm install`
+2. `npm run start`
+
+It will boot up an http server on port 3000.
+
+Have fun!
--- index.js
+++ index.js
@@ -3,8 +3,13 @@ require('./db/db');
 const userRoutes = require('./routes/user');
 const taskRoutes = require('./routes/task');
 const express = require('express');
+const cors = require('cors');
 const { PORT } = process.env;
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:4200'
+}));
 
 app.use(express.json());
 app.use(userRoutes);
